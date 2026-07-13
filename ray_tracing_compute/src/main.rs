88387f9868945be798469e9ed6ad7958e4f8b76a
William Switzer <snow2overlord@gmail.com>
2026-07-13 16:16:03 -0400

    Added a crate and started work on ray tracing with Vulkan

---
+++ ray_tracing_compute/src/main.rs
@@ -0,0 +1,62 @@
+use vulkano::VulkanLibrary;
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
+
+// Auto Format:
+//      Windows: Shift + Alt + F
+//      Mac: Shift + Option + F
+
+// Following: https://vulkano.rs/01-introduction/01-introduction.html
+
+fn main() {
+    initalization();
+}
+
+fn initalization() {
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+            ..Default::default()
+        },
+    )
+    .expect("failed to create instance");
+
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("Could note enumerate devices")
+        .next()
+        .expect("no devices available");
+
+    for family in physical_device.queue_family_properties() {
+        println!(
+            "Found a queue family with {:?} queue(s)",
+            family.queue_count
+        );
+    }
+
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .position(|queue_family_properties| {
+            queue_family_properties
+                .queue_flags
+                .contains(QueueFlags::GRAPHICS)
+        })
+        .expect("couldn't find a graphical queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+}
